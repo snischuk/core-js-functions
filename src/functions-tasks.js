65fec1efefd084bbc00096052cd107c4e494c13f
Snischuk Maxim <m.snischuk@gmail.com>
2024-03-23 23:39:35 +0200

    add task retry

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -140,8 +140,20 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    let attempt = 0;
+    let result = null;
+    while (attempt < attempts) {
+      try {
+        result = func();
+        return result;
+      } catch {
+        attempt += 1;
+      }
+    }
+    return result;
+  };
 }
 
 /**
