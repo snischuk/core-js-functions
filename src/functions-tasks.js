1e5b734c221b52f2de353a337e1a81439940bd24
Snischuk Maxim <m.snischuk@gmail.com>
2024-03-24 01:09:17 +0200

    add task getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -228,8 +228,14 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let currentId = startFrom;
+
+  return () => {
+    const result = currentId;
+    currentId += 1;
+    return result;
+  };
 }
 
 module.exports = {
